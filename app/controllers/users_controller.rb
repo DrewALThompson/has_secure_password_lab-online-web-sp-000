a7726af075be2098695e301e7dc0972995b76f28
Drew Thompson <drewallynthompson@gmail.com>
2020-12-08 08:39:49 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to :welcome
     else
       render :login, alert: "User Failed to Create"
@@ -19,6 +20,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-  params.require(:user).permit
+  params.require(:user).permit(:name, :password, :password_confirmation)
   end 
 end
